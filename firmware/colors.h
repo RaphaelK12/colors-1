756b23bbbbd0173359fe54052a94d1a8bee6a45f
stefan-3r <stefan.3r@gmx.de>
2016-10-23 17:59:34 +0200

    Create colors.h

---
+++ firmware/colors.h
@@ -0,0 +1,18 @@
+typedef struct {
+    double r;       // percent
+    double g;       // percent
+    double b;       // percent
+} rgb;
+
+typedef struct {
+    double h;       // angle in degrees
+    double s;       // percent
+    double v;       // percent
+} hsv;
+
+static hsv   rgb2hsv(rgb in);
+static rgb   hsv2rgb(hsv in);
+
+hsv rgb2hsv(rgb in);
+
+rgb hsv2rgb(hsv in);
